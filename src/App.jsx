3af84f9a939aaf11cd70c381ca93e71c6047d2fd
ArfiH <arfihussain27@gmail.com>
2024-06-16 12:26:58 +0530

    added posts and filter in mobile layout :sparkles:

--- src/App.jsx
+++ src/App.jsx
@@ -45,10 +45,7 @@ function App() {
         </div>
 
         <div id="create-form" style={{ zIndex: 2 }}>
-          <div
-            className="create-account"
-            style={{ margin: "auto" }}
-          >
+          <div className="create-account" style={{ margin: "auto" }}>
             <dialog
               id="modal-content"
               className="modal-content border-0"
@@ -78,8 +75,9 @@ function App() {
                 <span aria-hidden="true">×</span>
               </button>
               <div
-                id="modal-header" className="modal-header custom-modal-header justify-content-center mb-4 p-2"
-                style={{ backgroundColor: "#EFFFF4", color: "#008A45"}}
+                id="modal-header"
+                className="modal-header custom-modal-header justify-content-center mb-4 p-2"
+                style={{ backgroundColor: "#EFFFF4", color: "#008A45" }}
               >
                 Let's learn, share &amp; inspire each other with our passion for
                 computer engineering. Sign up now 🤘🏼
@@ -226,9 +224,7 @@ function App() {
                           Create Account
                         </button>
                         <div className="d-flex flex-column gap-2">
-                          <button
-                            className="btn d-flex align-items-center justify-content-center gap-2 button-signup-group"
-                          >
+                          <button className="btn d-flex align-items-center justify-content-center gap-2 button-signup-group">
                             <img
                               src="https://s3-alpha-sig.figma.com/img/2260/c71f/967377e16ffbb611ef03393e79e51f6e?Expires=1719187200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=TZdaqYj40GA4jv3EhToholknIWUtv8HxXMqxYOdS4OISvY6uLq2t8BhpMJt2PANl1js4wDfkB4VaKbFENKQu2FSWkANDGZEgkRpyuBOk-LuL2zvVdlAIsGrlBCZxk8JPfaEQ8a9mIC3zJscbXG66e5VPvyQ3LLbiu9zcOVaoMbBVy9I01dhhTkHCenDioEqOpglg-TykJHXFPBOkHX98SR4ClboNzRVUlu8eotyP9RQZ7h4y4cbMBSm79VdvaVRSsLCWfzrHKT7BwE1KYGxEMwTZo1iKCSLrKGXb391CC163TNSWSxuy~UMr0Tep9TnbbwwjvieMYxdlYIuWUeY4sw__"
                               alt="facebook"
@@ -237,9 +233,7 @@ function App() {
                             />
                             Sign Up with Facebook
                           </button>
-                          <button
-                            className="btn d-flex align-items-center gap-2 button-signup-group justify-content-center"
-                          >
+                          <button className="btn d-flex align-items-center gap-2 button-signup-group justify-content-center">
                             <img
                               src="data:image/svg+xml,%3csvg%20width='16'%20height='16'%20viewBox='0%200%2016%2016'%20fill='none'%20xmlns='http://www.w3.org/2000/svg'%3e%3cg%20clip-path='url(%23clip0_1_2217)'%3e%3cpath%20d='M3.54594%209.66905L2.989%2011.7482L0.953406%2011.7912C0.345063%2010.6629%200%209.37192%200%208.00005C0%206.67345%200.322625%205.42245%200.8945%204.32092H0.894938L2.70719%204.65317L3.50106%206.45455C3.33491%206.93895%203.24434%207.45895%203.24434%208.00005C3.24441%208.5873%203.35078%209.14995%203.54594%209.66905Z'%20fill='%23FBBB00'/%3e%3cpath%20d='M15.8602%206.50562C15.9521%206.98955%2016%207.48933%2016%208.00012C16%208.57287%2015.9398%209.13155%2015.8251%209.67046C15.4357%2011.5043%2014.4181%2013.1056%2013.0084%2014.2388L13.008%2014.2384L10.7253%2014.1219L10.4023%2012.1052C11.3377%2011.5566%2012.0687%2010.6981%2012.4537%209.67046H8.1759V6.50562H12.5161H15.8602Z'%20fill='%23518EF8'/%3e%3cpath%20d='M13.0081%2014.2382L13.0085%2014.2386C11.6375%2015.3406%209.89596%2015.9999%208.00015%2015.9999C4.95355%2015.9999%202.30477%2014.2971%200.953552%2011.7911L3.54608%209.66895C4.22168%2011.472%205.96102%2012.7555%208.00015%2012.7555C8.87662%2012.7555%209.69774%2012.5186%2010.4023%2012.105L13.0081%2014.2382Z'%20fill='%2328B446'/%3e%3cpath%20d='M13.1064%201.84175L10.5148%203.9635C9.78553%203.50769%208.92353%203.24438%208.00003%203.24438C5.91475%203.24438%204.14288%204.58678%203.50113%206.4545L0.894969%204.32088H0.894531C2.22597%201.75384%204.90816%200%208.00003%200C9.94112%200%2011.7209%200.691438%2013.1064%201.84175Z'%20fill='%23F14336'/%3e%3c/g%3e%3cdefs%3e%3cclipPath%20id='clip0_1_2217'%3e%3crect%20width='16'%20height='16'%20fill='white'/%3e%3c/clipPath%3e%3c/defs%3e%3c/svg%3e"
                               alt="google"
@@ -358,8 +352,23 @@ function App() {
         </div>
         <hr className="mx-auto mt-0" style={{ maxWidth: 1040 }} />
         <div className="container3">
+          <div id="mobile-posts-header" className="mx-auto my-1 gap-5 justify-content-between align-items-center">
+            <p className="posts-mobile">Posts (368)</p>
+            <button
+                className="btn"
+                aria-label="write-post"
+                style={{ background: "rgb(242, 242, 242)" }}
+              >
+                Filter
+                <img
+                  src="data:image/svg+xml,%3csvg%20width='22'%20height='22'%20viewBox='0%200%2022%2022'%20fill='none'%20xmlns='http://www.w3.org/2000/svg'%3e%3cg%20id='baseline-arrow_drop_down-24px'%20clip-path='url(%23clip0_1_1136)'%3e%3cpath%20id='Vector'%20d='M6.41667%209.16669L11%2013.75L15.5833%209.16669H6.41667Z'%20fill='black'/%3e%3c/g%3e%3cdefs%3e%3cclipPath%20id='clip0_1_1136'%3e%3crect%20width='22'%20height='22'%20fill='white'/%3e%3c/clipPath%3e%3c/defs%3e%3c/svg%3e"
+                  alt="arrow down"
+                />
+              </button>
+          </div>
           <div
-            className="mx-auto d-flex gap-5 justify-content-center"
+            id="main"
+            className="mx-auto d-flex justify-content-center"
             style={{ maxWidth: 1040 }}
           >
             <div className="posts-center position-relative">
